be3fa17e421af05dcd1f1f5e2a3b359c77fb5287
dolppe <dbtjdals9342@naver.com>
2024-01-19 16:47:50 +0900

    에셋 매니저 및 로그 채널 생성 #4

---
+++ Source/LyraBattleRoyalGame/BattleLogChannels.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Containers/UnrealString.h"
+#include "Logging/LogMacros.h"
+
+LYRABATTLEROYALGAME_API DECLARE_LOG_CATEGORY_EXTERN(LogBattle, Log, All);
+
---
+++ Source/LyraBattleRoyalGame/System/BattleAssetManager.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "BattleAssetManager.generated.h"
+
+UCLASS()
+class UBattleAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+public:
+
+	UBattleAssetManager();
+	
+	static UBattleAssetManager& Get();
+
+	// 에셋 캐싱
+	void AddLoadedAsset(const UObject* Asset);
+	
+	static bool ModuleTest();
+
+	static bool ShouldLogAssetLoads();
+
+	// 동기적인 정적 로딩
+	static UObject* SynchronousLoadAsset(const FSoftObjectPath& AssetPath);
+	
+	// BP Class / Object 등 에셋 정적 로딩
+	template<typename AssetType>
+	static AssetType* GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	// Class 정적 로딩
+	template<typename AssetType>
+	static TSubclassOf<AssetType> GetSubClass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	virtual void StartInitialLoading() override;
+	
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets;
+
+	// Object 단위 Locking
+	FCriticalSection SyncObject;
+	
+};
+
+template <typename AssetType>
+AssetType* UBattleAssetManager::GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	AssetType* LoadedAsset = nullptr;
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+	if (AssetPath.IsValid())
+	{
+		LoadedAsset = AssetPointer.Get();
+		if (!LoadedAsset)
+		{
+			LoadedAsset = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedAsset, TEXT("Failed to load asset [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedAsset && bKeepInMemory)
+		{
+			Get().AddLoadedAsset(Cast<UObject>(LoadedAsset));
+		}
+	}
+	return LoadedAsset;
+}
+
+template <typename AssetType>
+TSubclassOf<AssetType> UBattleAssetManager::GetSubClass(const TSoftClassPtr<AssetType>& AssetPointer,
+	bool bKeepInMemory)
+{
+	TSubclassOf<AssetType> LoadedSubclass;
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+	if (AssetPath.IsValid())
+	{
+		LoadedSubclass = AssetPointer.Get();
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<UClass>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedSubclass, TEXT("Failed to load asset [%s]"), *LoadedSubclass.ToString());
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+	return LoadedSubclass;
+}
