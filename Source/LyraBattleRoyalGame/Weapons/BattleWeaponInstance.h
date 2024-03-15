4a4170a7cbd387949cf38f87c6a15ba8faca1121
dolppe <dbtjdals9342@naver.com>
2024-03-15 18:56:11 +0900

    BP 로직으로 총기 장착, 애니메이션 적용 #34

--- Source/LyraBattleRoyalGame/Weapons/BattleWeaponInstance.h
+++ Source/LyraBattleRoyalGame/Weapons/BattleWeaponInstance.h
@@ -10,6 +10,9 @@ class UBattleWeaponInstance : public UBattleEquipmentInstance
 	GENERATED_BODY()
 public:
 	UBattleWeaponInstance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UFUNCTION(BlueprintCallable, BlueprintPure=false, Category=Animation)
+	TSubclassOf<UAnimInstance> PickBestAnimLayer(bool bEquipped, const FGameplayTagContainer& CosmeticTags)const;
 	
 	// Weapon에 Equip, UnEquip에 대한 Animation Set 정보를 들고 있음.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Animation)
